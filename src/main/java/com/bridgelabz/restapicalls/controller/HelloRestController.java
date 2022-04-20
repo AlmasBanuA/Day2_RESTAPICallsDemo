b190571351294b56e4768df2dd0b57cf6014b0af
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-04-20 20:30:20 +0530

    UPDATED: rest call to show message passing firstName and lastName in

--- src/main/java/com/bridgelabz/restapicalls/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicalls/controller/HelloRestController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.restapicalls.controller;
 
+import com.bridgelabz.restapicalls.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +20,10 @@ public class HelloRestController {
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello "+name+" From BridgeLabz";
+    }
+
+    @PostMapping("/post")
+    public String userData(@RequestBody User user) {
+        return "Hello "+user.getfName()+" "+user.getlName()+" from BridgeLabz";
     }
 }
